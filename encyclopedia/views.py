dfb681371b07e6c5c4093f1e2f0f6c4992cf2f14
JuanPM <jua6n@yahoo.com>
2021-02-12 22:24:38 +0100

    Enable dynamic view generation for entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
+
+
+def display_entry(request, entry_title):
+    entry_md = util.get_entry(entry_title)
+    entry_html = markdown2.markdown(entry_md)
+    return render(request, "encyclopedia/entry.html", {
+        "entry_title": entry_title,
+        "entry": entry_html})
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
