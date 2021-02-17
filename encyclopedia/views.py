4631c027c3f270d4ccc437df63bfafd156095a57
JuanPM <jua6n@yahoo.com>
2021-02-18 00:50:03 +0100

    All good

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ import re
 import random
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotAllowed
+from django import forms
 import markdown2
 
 from . import util
@@ -11,28 +12,6 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
-
-def search(request):
-    search_term = request.GET['q']
-    existing_entries = util.list_entries()
-    exact_match = [x for x in existing_entries if x.lower() ==
-                   search_term.lower()]
-    if exact_match:
-        exact_match = exact_match[0]
-    partial_matches = [
-        x for x in existing_entries if search_term.lower() in x.lower()]
-
-    if exact_match:
-        return display_entry(request, exact_match)
-    elif partial_matches:
-        return render(request, "encyclopedia/partial_matches.html", {
-            "partial_matches": partial_matches,
-            "search_term": search_term
-        })
-    else:
-        # TODO: Add message to alert user his search did not return any results
-        return index(request)
 
 
 def display_entry(request, entry_title):
@@ -45,6 +24,27 @@ def display_entry(request, entry_title):
     return render(request, "encyclopedia/display_entry.html", {
         "entry_title": entry_title,
         "entry": entry_html})
+
+
+def search(request):
+    search_term = request.GET['q']
+    existing_entries = util.list_entries()
+
+    if exact_match := [x for x in existing_entries if x.lower() ==
+                       search_term.lower()]:
+        exact_match = exact_match[0]
+        return display_entry(request, exact_match)
+
+    elif partial_matches := [
+            x for x in existing_entries if search_term.lower() in x.lower()]:
+        return render(request, "encyclopedia/partial_matches.html", {
+            "partial_matches": partial_matches,
+            "search_term": search_term
+        })
+
+    else:
+        # TODO: Add message to alert user his search did not return any results
+        return index(request)
 
 
 def new_entry(request):
